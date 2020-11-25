757966f376a629f2c205478dd506d1686767db63
Olga Sinenkova <osinenkova@OSINENKOVA.ps.lan>
2020-11-25 02:26:11 +0100

    upd added a list generetad through redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,41 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// REDUX
+import {createStore} from 'redux';
+
+function playlist (store= [], action) {
+  if(action.type === 'ADD_TRACK') {
+    return [
+      ...store,
+      action.payload
+    ];
+  }
+  return store;
+}
+
+const store = createStore(playlist);
+const addTrackBtn = document.querySelectorAll('.add-track')[0];
+const input = document.querySelectorAll('.track-input')[0];
+
+store.subscribe(()=>{
+  console.log('subscribe', store.getState());
+  const list = document.querySelector('.list');
+  list.innerHTML = '';
+  input.value = '';
+  store.getState().forEach(element => {
+    const li = document.createElement('li');
+    li.textContent = element;
+    list.appendChild(li);
+  });
+})
+
+addTrackBtn.addEventListener('click', () => {
+  const trackName = input.value;
+  // console.log('trackName', trackName)
+  store.dispatch({type: 'ADD_TRACK', payload: trackName})
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
